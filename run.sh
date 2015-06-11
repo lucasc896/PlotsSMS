8ab771504aea7de4565a33e3a97d4b5d5e273811
bainbrid <robert.bainbridge@cern.ch>
2015-06-11 11:46:22 +0200

    updated to reorganised configs

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
-#python python/makeSMSplots.py config/SUS12024/T1tttt_SUS12024.cfg T1ttttRA2b
-#python python/makeSMSplots.py config/SUS14006/T2cc_SUS14006_yossof.cfg T2cc
-python python/makeSMSplots.py config/SUS14006/T2cc_SUS14006.cfg T2cc
-#python python/makeSMSplots.py config/SUS14006/T2degen_SUS14006.cfg T2degen
+# Yossof limits
+python python/makeSMSplots.py config/SUS14006/yossof/T2cc/T2cc_SUS14006.cfg T2cc_yossof_
+
+# Excess limits (unknown source, but early)
+python python/makeSMSplots.py config/SUS14006/excess/T2cc/T2cc_SUS14006.cfg T2cc_excess_
+python python/makeSMSplots.py config/SUS14006/excess/T2_4body/T2degen_SUS14006.cfg T2degen_excess_
+python python/makeSMSplots.py config/SUS14006/excess/T2_4body_boosted/T2degen_SUS14006.cfg T2degen_boosted_excess_
+
+# Toys limits (from Rob, unknown version, but recent)
+python python/makeSMSplots.py config/SUS14006/toys/T2cc/T2cc_SUS14006.cfg T2cc_toys_
+python python/makeSMSplots.py config/SUS14006/toys/T2_4body/T2degen_SUS14006.cfg T2degen_toys_
+
+# Chris limits (30th Mar, asymptotic CLs)
+python python/makeSMSplots.py config/SUS14006/chris/T2cc/T2cc_SUS14006.cfg T2cc_chris_asymp_
+python python/makeSMSplots.py config/SUS14006/chris/T2_4body/T2degen_SUS14006.cfg T2degen_chris_asymp_
+
+# Latest limits (these should be used for paper)
+python python/makeSMSplots.py config/SUS14006/latest/T2cc/T2cc_SUS14006.cfg T2cc_latest_
+python python/makeSMSplots.py config/SUS14006/latest/T2_4body/T2degen_SUS14006.cfg T2degen_latest_
